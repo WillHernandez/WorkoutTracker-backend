689d2fb7c5e48c14561257d522ff4147ef25d230
Will Hernandez <w.hernandez.code@gmail.com>
2022-08-04 09:55:28 -0400

    Cors access updated

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
+const cors = require('cors');
 require('dotenv').config();
 
 app.use(express.json());
+
+const whitelist = [process.env.LOCALHOST];
+const corsOptions = {
+	origin: (origin, callback) => {
+		if(whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
 	console.log(`${req.path}: ${req.method}`);
